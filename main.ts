e5c7385d6af6d718cf149aa144c8262b7c4c409b
Anson Horton <ansonh@microsoft.com>
2018-12-29 07:42:49 -0800

    PowerMeter
    * Add PowerMeter class
    * Put in basics for bouncing logic as scratch work

--- main.ts
+++ main.ts
@@ -7,20 +7,26 @@ enum SpriteKind {
 
 let golfBallSprite: Sprite = null;
 const golfer = new Golfer();
+const powerMeter = new PowerMeter(32, 8, 15, 4);
 
 controller.A.onEvent(ControllerButtonEvent.Pressed, () => {
-    golfer.swing(() => {
-        music.golfBallHit.play();
-        golfBallSprite.vx = 45;
-        golfBallSprite.vy = -50;
-        golfBallSprite.ay = 9.81;
-        golfBallSprite.ax = 2;
-    });
+    if (powerMeter.isRunning) {
+        let power = powerMeter.stop();
+    } else {
+        powerMeter.start(16, 160);
+    }
+    // golfer.swing(() => {
+    //     music.golfBallHit.play();
+    //     golfBallSprite.vx = 42;
+    //     golfBallSprite.vy = -50;
+    //     golfBallSprite.ay = 9.81;
+    //     golfBallSprite.ax = 0;
+    // });
 });
 
 const targetingAideImageSize = 48;
 const targetingAideMidpoint = targetingAideImageSize / 2;
-const targetingAideSize = 3;
+const targetingAideSize = 2;
 const verticalTargetingAide = image.create(targetingAideImageSize, targetingAideImageSize);
 const targetingAideSprite = sprites.create(verticalTargetingAide);
 targetingAideSprite.z = 2;
@@ -34,7 +40,11 @@ const targetingAidePoints = [
     { x: -targetingAideMidpoint + targetingAideSize, y: -targetingAideSize }
 ];
 
+
+
 game.currentScene().eventContext.registerFrameHandler(19, () => {
+
+
     if ((controller.left.isPressed() && angle >= 0) || (controller.right.isPressed() && angle <= 180)) {
         controller.left.isPressed() ? angle-- : angle++;
         angle > 90 ? golfer.setOrientation(GolferOrientation.Right) : golfer.setOrientation(GolferOrientation.Left);
@@ -57,10 +67,10 @@ game.currentScene().eventContext.registerFrameHandler(19, () => {
         verticalTargetingAide.fill(0);
 
         // Draw the 4 lines of the targeting aide
-        verticalTargetingAide.drawLine(targetingAideMidpoint, targetingAideMidpoint, points[0].x, points[0].y, 3);
-        verticalTargetingAide.drawLine(points[1].x, points[1].y, points[2].x, points[2].y, 3);
-        verticalTargetingAide.drawLine(points[1].x, points[1].y, points[0].x, points[0].y, 3);
-        verticalTargetingAide.drawLine(points[2].x, points[2].y, points[0].x, points[0].y, 3);
+        verticalTargetingAide.drawLine(targetingAideMidpoint, targetingAideMidpoint, points[1].x, points[1].y, 4);
+        verticalTargetingAide.drawLine(targetingAideMidpoint, targetingAideMidpoint, points[2].x, points[2].y, 4);
+        verticalTargetingAide.drawLine(points[1].x, points[1].y, points[0].x, points[0].y, 4);
+        verticalTargetingAide.drawLine(points[2].x, points[2].y, points[0].x, points[0].y, 4);
     }
 });
 
@@ -148,5 +158,20 @@ golfBallSprite = sprites.create(img`
 
 golfBallSprite.setPosition(13, 206);
 golfBallSprite.z = 1;
-golfBallSprite.setFlag(SpriteFlag.BounceOnWall, true)
 scene.cameraFollowSprite(golfBallSprite);
+
+// coefficient of restituion for grass
+const grassSurfaceCOR = 0.5;
+
+scene.onHitTile(SpriteKind.Projectile, 7, (sprite: Sprite) => {
+    sprite.vx *= grassSurfaceCOR;
+    sprite.vy *= grassSurfaceCOR;
+
+    if (sprite.isHittingTile(CollisionDirection.Left) || sprite.isHittingTile(CollisionDirection.Right)) {
+        sprite.vx = -sprite.vx;
+    }
+
+    if (sprite.isHittingTile(CollisionDirection.Top) || sprite.isHittingTile(CollisionDirection.Bottom)) {
+        sprite.vy = -sprite.vy;
+    }
+})
